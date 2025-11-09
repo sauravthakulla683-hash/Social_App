3dead85c751d01cfdb7785bfb9d28b70261b6a5e
Saurabh <sauravthakulla683@gmail.com>
2025-11-09 19:42:04 +0530

    Models part done

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,15 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const mongodb = require("./conn/conn");
+const auth = require("./routes/auth");
 mongodb();
 
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello backend working");
 });
+app.use("/api/login", auth);
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server Running at ${port}`));
---
+++ backend/model/posts.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  caption: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+module.exports = mongoose.model("Post", postSchema);
