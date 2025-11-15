c9ce724573b4d3bbc382e1e97fa9f1e173d5ee2c
Saurabh <sauravthakulla683@gmail.com>
2025-11-15 23:06:30 +0530

    Login created succesfully

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -25,4 +25,25 @@ router.post("/signin", async (req, res) => {
 });
 
 //Login
+router.post("/login", async (req, res) => {
+  try {
+    let { email, password } = req.body;
+    let user = await User.findOne({ email });
+    if (user) {
+      let isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ message: "Incorrect password" });
+      } else {
+        res
+          .status(200)
+          .json({ message: "Login succesfull", email: user.email });
+      }
+    } else {
+      return res.send("User not found");
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+});
+
 module.exports = router;
